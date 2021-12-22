f2f27cb97129f0f45dc660d0de877b213d68c7e4
sharma0611 <sharma.shivam0611@gmail.com>
2021-12-22 11:56:20 -0500

    change to other

--- functions/integrations/slack.js
+++ functions/integrations/slack.js
@@ -1,12 +1,10 @@
 const { App } = require("@slack/bolt");
 
-console.log("secret ", process.env.SLACK_SIGNING_SECRET);
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
-
 const sendSlackMessage = (userId, message) => {
+  const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  });
   return app.client.chat.postMessage({ channel: userId, text: message });
 };
 
